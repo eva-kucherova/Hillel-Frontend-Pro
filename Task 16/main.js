1c103f82fbdf083f8c728dd76c02caf4b75a5b70
Eva Kucherova <evahekate@yahoo.com>
2024-02-25 21:42:04 +0200

    ready for tests

--- Task 16/main.js
+++ Task 16/main.js
@@ -19,3 +19,90 @@ alert('Hello Eva! Task 16');
 
 // Не забудьте після того, як напишите цей конструктор, створити 2 - 3 екземпляри(конкретних студентів) і
 // показати використання цих методів.
+
+class Student {
+  constructor(name, surname, birthYear, marks) {
+    this.name = name;
+    this.surname = surname;
+    this.birthYear = birthYear;
+    this.marks = marks;
+  }
+
+  calculateAge() {
+    let today = new Date();
+    let currentYear = today.getFullYear();
+    let studentAge = Number(currentYear) - Number(this.birthYear);
+    console.log(studentAge);
+  }
+
+  attendance = new Array(25);
+
+  present() {
+    const next_index = this.attendance.findIndex((x) => x === undefined);
+    if (next_index != -1) {
+      this.attendance[next_index] = true;
+    }
+    console.log(this.attendance);
+  }
+  absent() {
+    const next_index = this.attendance.findIndex((x) => x === undefined);
+    if (next_index != -1) {
+      this.attendance[next_index] = false;
+    }
+  }
+
+  getAverageMark() {
+    let sum = this.marks.reduce(function (accum, mark) {
+      return accum + mark;
+    }, 0);
+    const averageMark = Math.round(sum / this.marks.length);
+    console.log(averageMark);
+    return averageMark;
+  }
+
+  getAverageAttendance() {
+    const visitedDays = this.attendance.filter((elem) => elem).length;
+    const totalDays = this.attendance.filter(
+      (elem) => elem !== undefined
+    ).length;
+    const averageAttendance = (visitedDays / totalDays).toFixed(1);
+    console.log(averageAttendance);
+    return averageAttendance;
+  }
+
+  summary() {
+    const averageMark = this.getAverageMark();
+    const averageAttendance = this.getAverageAttendance();
+    switch (true) {
+      case averageMark > 90 && averageAttendance > 0.9:
+        console.log('Молодець!');
+        break;
+      case (averageMark >= 90 && averageAttendance <= 0.9) ||
+        (averageMark <= 90 && averageAttendance >= 0.9):
+        console.log('Добре, але можна краще');
+        break;
+      case averageMark < 90 && averageAttendance < 0.9:
+        console.log('Редиска!');
+        break;
+    }
+  }
+}
+
+const student1 = new Student(
+  'Eva',
+  'Kucherova',
+  '1979',
+  [100, 95, 98, 100, 84, 96, 100, 94]
+);
+student1.calculateAge();
+student1.getAverageMark();
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+console.log(student1.attendance);
+
+student1.getAverageAttendance();
+student1.summary();
