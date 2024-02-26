4e43ea8b15c66be4ae7c0e618dc3b65cd0e5a092
Eva Kucherova <evahekate@yahoo.com>
2024-02-26 12:35:15 +0200

    finished

--- Task 18/main.js
+++ Task 18/main.js
@@ -1,7 +1,5 @@
 // --- Task 18 ---
 
-alert('Hello Eva! Task 18');
-
 // Створити масив, довжину та елементи якого задає користувач.
 
 // Відсортувати масив за зростанням.
@@ -9,3 +7,32 @@ alert('Hello Eva! Task 18');
 // Видалити елементи з масиву з 2 по 4 (включно!).
 
 // У міру змін виводити вміст масиву на сторінку.
+
+const numOfElements = Number(prompt('Enter the number of elements: '));
+let userArray = new Array(numOfElements);
+let numIndex = 0;
+do {
+  const userNumber = Number(prompt('Enter the number: '));
+  userArray[numIndex] = userNumber;
+  numIndex++;
+} while (numIndex < numOfElements);
+alert(`Thanks! Let's look at result`);
+
+console.log(userArray);
+document.getElementById('user_array').innerHTML = userArray;
+
+const sortedArray = userArray.sort(function (a, b) {
+  return a - b;
+});
+console.log(sortedArray);
+document.getElementById('sorted_array').innerHTML = sortedArray;
+
+if (userArray.length >= 5) {
+  sortedArray.splice(2, 3);
+  document.getElementById('cutted_array').innerHTML = sortedArray;
+  console.log(sortedArray);
+} else {
+  document.getElementById(
+    'cutted_array'
+  ).innerHTML = `Sorry, your array is too short`;
+}
