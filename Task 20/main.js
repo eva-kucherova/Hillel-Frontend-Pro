e64e9690bfda407b5877637d2f401e0f5e7f2edc
Eva Kucherova <evahekate@yahoo.com>
2024-02-26 13:21:52 +0200

    finished

--- Task 20/main.js
+++ Task 20/main.js
@@ -17,4 +17,13 @@ function generateKey(num, chars) {
 }
 
 const key = generateKey(16, characters);
+const key1 = generateKey(8, characters);
+const key2 = generateKey(25, characters);
+const key3 = generateKey(12, characters);
+const key4 = generateKey(6, characters);
+
 console.log(key); // eg599gb60q926j8i
+console.log(key1);
+console.log(key2);
+console.log(key3);
+console.log(key4);
