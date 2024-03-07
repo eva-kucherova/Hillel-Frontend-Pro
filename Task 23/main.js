a806bc280bc8f2a18a24940a57ada06806417553
Eva Kucherova <evahekate@yahoo.com>
2024-03-07 16:21:56 +0200

    finished tested

--- Task 23/main.js
+++ Task 23/main.js
@@ -4,3 +4,21 @@ alert('hello Eva! Task 23');
 
 // Є текстове поле на сторінці.Під час фокусування на цьому полі збоку з'являється <div>.
 //  При пропажі фокусу - <div> так само пропадає
+const form = document.getElementById('form');
+const ghostDiv = document.getElementById('ghost');
+
+form.addEventListener(
+  'focus',
+  (event) => {
+    ghostDiv.style.visibility = 'visible';
+  },
+  true
+);
+
+form.addEventListener(
+  'blur',
+  (event) => {
+    ghostDiv.style.visibility = 'hidden';
+  },
+  true
+);
