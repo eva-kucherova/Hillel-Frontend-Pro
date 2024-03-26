b2289042ce5462e8856c6cc3ccb9d61171d93f59
Eva Kucherova <evahekate@yahoo.com>
2024-03-26 13:38:57 +0200

    finished

--- Task 30/main.js
+++ Task 30/main.js
@@ -13,3 +13,31 @@
 // deg - напрям у градусах
 // icon - значок, де 10d код іконки
 // http://openweathermap.org/img/w/10d.png
+
+const weatherDatas = document.getElementById('div');
+const header = document.getElementById('header');
+const xml = new XMLHttpRequest();
+
+xml.open(
+  'GET',
+  'http://api.openweathermap.org/data/2.5/weather?q=SOFIA&units=metric&APPID=5d066958a60d315387d9492393935c19'
+);
+
+xml.send();
+xml.addEventListener('load', (event) => {
+  const response = JSON.parse(event.target.response);
+  header.innerHTML = `Weather in ${response.name}`;
+  weatherDatas.innerHTML = '';
+  weatherDatas.innerHTML += `Temperature: ${response.main.temp} °C` + '<br>';
+  weatherDatas.innerHTML += `Pressure: ${response.main.pressure} hPa` + '<br>';
+  weatherDatas.innerHTML +=
+    `Description: ${response.weather[0].description}` + '<br>';
+  +'<br>';
+  weatherDatas.innerHTML += `Humidity: ${response.main.humidity} %` + '<br>';
+  weatherDatas.innerHTML += `Wind speed: ${response.wind.speed} km/h` + '<br>';
+  weatherDatas.innerHTML += `Wind degree: ${response.wind.deg} °` + '<br>';
+  const icon = document.createElement('img');
+  icon.src = 'http://openweathermap.org/img/w/01d.png';
+  const container = document.querySelector('#div');
+  container.append(icon);
+});
